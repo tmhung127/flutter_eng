efa19794fb337b15697e55b48fa839f97aa0db1a
tmhung127 <91183039+tmhung127@users.noreply.github.com>
2022-09-12 20:22:51 +0700

    1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -92,7 +92,7 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: AppColors.secondColor,
         elevation: 0,
         title: Text(
-          'English 1 ju',
+          'English 423423 ju',
           style: AppStyles.h3.copyWith(color: AppColors.greyText, fontSize: 36),
         ),
         leading: InkWell(
